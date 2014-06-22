dae2169f47554291299a001a755a675f85e0a221
ishikawakenji <k.ishikawa1006@gmail.com>
2014-06-22 14:09:25 +0900

    change LoadRecipeFile returns loaded recipe-data to displayName.java

--- src/fileIO/LoadRecipeFile.java
+++ src/fileIO/LoadRecipeFile.java
@@ -5,10 +5,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class LoadRecipeFile {
 	
-	public LoadRecipeFile (String _filePath){ 
+	public LoadRecipeFile (final String _filePath){ 
+		
+	}
+	
+	public static ArrayList<String> load (final String _filePath){ 
+		//ファイルの読み込みを行なうメソッド
+		//引数で指定されたファイルを一行ずつ読み込み、ArrayList<String>型として返す
+		
+		ArrayList<String> recipeDataArray = new ArrayList<String>();
 		
 		try{
 			//ファイルの読み込み
@@ -24,7 +33,15 @@ public class LoadRecipeFile {
 				if(recipe_name == null){
 					break;
 				}
-				System.out.println(recipe_name);
+				
+				//すぐに出力せずに、配列として保持し呼び出し側のクラスに渡すように変更
+				//レシピをIDなどと紐付けて管理するため
+				//change ishikawakenji//////////////
+				recipeDataArray.add(recipe_name);
+				////////////////////////////////////
+				//System.out.println(recipe_name);
+				////////////////////////////////////
+				
 			}
 			bf.close();
 		}catch(FileNotFoundException e){
@@ -33,5 +50,7 @@ public class LoadRecipeFile {
 			System.err.println("入出力エラーです");
 		}
 		
+		//add ishikawakenji/////////////////
+		return recipeDataArray;
 	}
 }
--- src/sezemi/displayName.java
+++ src/sezemi/displayName.java
@@ -1,12 +1,30 @@
 package sezemi;
 
+import java.util.ArrayList;
+
 import fileIO.LoadRecipeFile;
 
-public class displayName {
+public class displayName { 
+	
+	
 
 	public static void main(String[] args) {
 		//recipe-data.txtを一行ずつ読み込み、その中のレシピの名前を標準出力する
-		LoadRecipeFile _lrf = new LoadRecipeFile("recipe-data.txt");
+		//LoadRecipeFile _lrf = new LoadRecipeFile("recipe-data.txt");
+		
+		ArrayList<String> recipeDataArray = new ArrayList<String>();
+		recipeDataArray = LoadRecipeFile.load("recipe-data.txt");
+		
+		printRecipeData(recipeDataArray);
+	}
+	
+	static void printRecipeData(ArrayList<String> _array) {
+		int i;
+		int len = _array.size();
+		
+		for(i=0; i<len; i++) {
+			System.out.println(_array.get(i));
+		}
 	}
 
 }
