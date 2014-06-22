aaf2eadedc4ecbbd7901daa7cde111af2ba561e6
Kentarou Makihara <makihara.k.aa@m.titech.ac.jp>
2014-06-22 11:45:48 +0900

    spec3:とりあえず動く

--- src/sezemi/displayName.java
+++ src/sezemi/displayName.java
@@ -1,9 +1,24 @@
 package sezemi;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class displayName {
 
 	public static void main(String[] args) {
-		System.out.println("オムライス");
+		try{
+			File f = new File("recipe-data.txt");
+			BufferedReader bf = new BufferedReader(new FileReader(f));
+			String recipe_name = bf.readLine();
+			System.out.println(recipe_name);
+		}catch(FileNotFoundException e){
+			System.err.println("ファイルが見つかりません");
+		}catch(IOException e){
+			System.err.println("入出力エラーです");
+		}
 	}
 
 }
