52b59e993b631f1916e0b48b4a7d5abad8eefbd1
Kentarou Makihara <makihara.k.aa@m.titech.ac.jp>
2014-06-22 11:50:37 +0900

    spec3:コメントを追加

--- src/sezemi/displayName.java
+++ src/sezemi/displayName.java
@@ -9,11 +9,16 @@ import java.io.IOException;
 public class displayName {
 
 	public static void main(String[] args) {
+		//recipe-data.txtを読み込み、その中のレシピの名前を標準出力する
 		try{
+			//ファイルの読み込み
 			File f = new File("recipe-data.txt");
 			BufferedReader bf = new BufferedReader(new FileReader(f));
+			
+			//読み込んだレシピ名の出力
 			String recipe_name = bf.readLine();
 			System.out.println(recipe_name);
+			bf.close();
 		}catch(FileNotFoundException e){
 			System.err.println("ファイルが見つかりません");
 		}catch(IOException e){
